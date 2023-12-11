317a476c9ce1d1f92f994d514884a0b1adf0caaa
raymond <stancillousray@gmail.com>
2023-12-11 22:15:28 +0300

    create an async routine

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Write an asynchronous coroutine named wait_random
+that waits for a random delay between 0 and max_delay
+seconds and eventually returns it.
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """async func that takes an arg, waits for  a random delay,
+    and then returns it"""
+    delay_time = random.uniform(0, max_delay)
+    await asyncio.sleep(delay_time)
+    return delay_time
