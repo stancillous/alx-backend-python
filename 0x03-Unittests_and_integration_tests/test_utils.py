7dd4b670a6d29f695530ff7cef5d9a45b6d89701
raymond <stancillousray@gmail.com>
2024-01-07 12:59:47 +0300

    unit test for the get_json func

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,9 +3,13 @@
 implement a class that will be used for
 writing unittests for our code
 """
+import requests
 from unittest import TestCase
+from unittest.mock import MagicMock, patch, Mock
 from parameterized import parameterized
+
 access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
 
 
 class TestAccessNestedMap(TestCase):
@@ -30,3 +34,23 @@ class TestAccessNestedMap(TestCase):
         """method to test that appropriate exception is raised"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(TestCase):
+    """class to test our get json func"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, payload):
+        """test our get json func returns correct result"""
+        mock_response = Mock()
+        mock_response.json.return_value = payload
+
+        with patch("requests.get", return_value=mock_response) as mock_method:
+            result = get_json(url)
+            print("\n\n\tres is ", result)
+            self.assertEqual(result, payload)
+
+        mock_method.assert_called_once_with(url)
