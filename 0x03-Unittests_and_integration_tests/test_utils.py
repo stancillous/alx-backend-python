7d2281b9d1c3e7b509f70e43f53c39c5866e9474
raymond <stancillousray@gmail.com>
2024-01-07 12:04:47 +0300

    write func to assert an error is raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,12 @@ class TestAccessNestedMap(TestCase):
     def test_access_nested_map(self, nested_map, path, expected_output):
         """test that the method works as expected"""
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """method to test that appropriate exception is raised"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
