f4de2c19a46118617d4a64d6605d37b0b8ef1f92
raymond <stancillousray@gmail.com>
2024-01-07 14:52:35 +0300

    tests for the org method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,7 @@ writing unittests for our funcs in the client module
 """
 from unittest import TestCase
 from parameterized import parameterized
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, Mock
 from typing import Dict
 GithubOrgClient = __import__('client').GithubOrgClient
 
@@ -18,19 +18,13 @@ class TestGithubClient(TestCase):
          ("abc", {"user": "stancillous"})
     ])
     @patch("client.get_json")
-    def test_org(self, url: str, expected_output: Dict) -> None:
+    def test_org(self, url: str, expected_output: Dict,
+                 mocked_method: MagicMock) -> None:
         """func to test the function works as expected"""
-        mocked_response = MagicMock()
-        mocked_response.json.return_value = expected_output
+        mocked_method.return_value = MagicMock(
+            return_value=expected_output)
         gh_client = GithubOrgClient(url)
         self.assertEqual(gh_client.org(), expected_output)
-        mocked_response.assert_called_once_with(
-            f"https://api.github.com.orgs/{url}"
+        mocked_method.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(url)
         )
-        # mocked_function.return_value = MagicMock(
-        #     return_value=expected_output)
-        # goclient = GithubOrgClient(origin)
-        # self.assertEqual(goclient.org(), expected_output)
-        # mocked_function.assert_called_once_with(
-        #     "https://api.github.com/orgs/{}".format(origin)
-        # )
