8b8f4fe86a317a49063d23d9611c898efde775e4
raymond <stancillousray@gmail.com>
2024-01-09 00:39:58 +0300

    add two more tests for our client functions

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,7 @@ writing unittests for our funcs in the client module
 """
 from unittest import TestCase
 from parameterized import parameterized
-from unittest.mock import patch, MagicMock, Mock
+from unittest.mock import patch, MagicMock, Mock, PropertyMock
 from typing import Dict
 GithubOrgClient = __import__('client').GithubOrgClient
 
@@ -28,3 +28,31 @@ class TestGithubClient(TestCase):
         mocked_method.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(url)
         )
+
+    def test_public_repos_url(self) -> None:
+        """method to test our _public_repos_url function
+        """
+
+        with patch(
+            "client.GithubOrgClient.org",
+            new_callable=PropertyMock,
+        ) as mock_org:
+
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
+
+    @parameterized.expand([
+        ({'license': {'key': "bsd-3-clause"}}, "bsd-3-clause", True),
+        ({'license': {'key': "bsl-1.0"}}, "bsd-3-clause", False),
+    ])
+    def test_has_license(self, repo: Dict, key: str, expected: bool) -> None:
+        """Testing our has_license method"""
+        gh_org_client = GithubOrgClient("google")
+        client_has_licence = gh_org_client.has_license(repo, key)
+        self.assertEqual(client_has_licence, expected)
